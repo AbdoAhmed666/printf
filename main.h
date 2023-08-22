ac48b627dbc9297d02df5fd5738f1936ce7ef385
Abdo <abdelrhman555@gmail.com>
2023-08-22 15:40:03 -0700

    commit printf

--- main.h
+++ main.h
@@ -1,7 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
-int _putchar(char c);
+
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+* struct cnvert - structure  functions with symbols
+*
+* @op: opereration
+* @funct: function assessment
+*/
+struct cnvert
+{
+	char *op;
+	int (*funct)(va_list);
+};
+typedef struct cnvert cnvert_t;
+
+int pars(const char *form, cnvert_t f_lst[], va_list arg_lst);
 int _printf(const char *format, ...);
+int _putchar(char c);
+int _print_char(va_list);
+int _print_strng(va_list);
+int _print_percent(va_list);
+
 
 #endif
--- printf.c
+++ printf.c
@@ -1,91 +1,162 @@
-#include"main.h"
-#include<stdarg.h>
+#include "main.h"
+#include <stdarg.h>
+#include <stddef.h>
 
 /**
- * _printf - Prints a formatted string to the standard output.
- * @format: A pointer to a character string.
+ * print_char - print a single character
+ * @args: the va_list containing the character to print
  *
- * Return: The number of characters printed (excluding the null byte used to
- * end output to strings).
+ * Return: the number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_string - print a string of characters
+ * @args: the va_list containing the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char*);
+	int len = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[len])
+	{
+		_putchar(str[len]);
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_number - print an integer number
+ * @args: the va_list containing the number to print
+ *
+ * Return: the number of characters printed
+ */
+int print_number(va_list args)
+{
+	int num = va_arg(args, int);
+	int len = 0;
+	int divisor = 1;
+	char buffer[20];
+
+	if (num < 0)
+	{
+		_putchar('-');
+		len++;
+		num *= -1;
+	}
+
+	while (num / divisor > 9)
+		divisor *= 10;
+
+	while (divisor > 0)
+	{
+		buffer[len] = (num / divisor) + '0';
+		_putchar(buffer[len]);
+		len++;
+		num %= divisor;
+		divisor /= 10;
+	}
+
+	return (len);
+}
+
+/**
+ * print_binary - print an unsigned int as binary
+ * @args: the va_list containing the number to print
+ *
+ * Return: the number of characters printed
+ */
+int print_binary(va_list args)
+{
+	unsigned int num = va_arg(args, unsigned int);
+	int len = 0;
+	int i;
+	char buffer[32];
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	for (i = 0; i < 32; i++)
+		buffer[i] = (num >> (31 - i) & 1) + '0';
+
+	for (i = 0; i < 32; i++)
+	{
+		if (buffer[i] == '1')
+			break;
+	}
+
+	for (; i < 32; i++)
+	{
+		_putchar(buffer[i]);
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: the format string
+ *
+ * Return: the number of characters printed
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int count = 0, digit, sign, len, num, i;
-	char buffer[20];
-	char c;
-	char *s;
+	int len = 0, i = 0;
 
-	if (format == NULL)
-		return (-1);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
 	va_start(args, format);
-	while (*format != '\0')
+	while (format[i])
 	{
-		if (*format == '%')
+		if (format[i] == '%')
 		{
-			format++;
-			if (*format == '%')
+			i++;
+			if (format[i] == '\0')
+				break;
+			if (format[i] == '%')
 			{
-				putchar('%');
-				count++;
+				_putchar('%');
+				len++;
 			}
-			else if (*format == 'c')
+			else if (format[i] == 'c')
+				len += print_char(args);
+			else if (format[i] == 's')
+				len += print_string(args);
+			else if (format[i] == 'd' || format[i] == 'i')
+				len += print_number(args);
+			else if (format[i] == 'b')
+				len += print_binary(args);
+			else
 			{
-				c = (char) va_arg(args, int);
-				putchar(c);
-				count++;
+				_putchar('%');
+				_putchar(format[i]);
+				len += 2;
 			}
-			else if (*format == 's')
-			{
-				s = va_arg(args, char *);
-				while (*s != '\0')
-				{
-					putchar(*s);
-					count++;
-					s++;
-				}
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				num = va_arg(args, int);
-				sign = 1;
-				if (num < 0)
-				{
-					sign = -1;
-					num = -num;
-				}
-				len = 0;
-				do {
-					digit = num % 10;
-					buffer[len] = digit + '0';
-					len++;
-					num /= 10;
-				} while (num > 0);
-				if (sign == -1)
-				{
-					putchar('-');
-					count++;
-				}
-				for (i = len - 1; i >= 0; i--)
-				{
-					putchar(buffer[i]);
-					count++;
-				}
-			}
-			format++;
 		}
 		else
 		{
-			putchar(*format);
-			count++;
-			format++;
+			_putchar(format[i]);
+			len++;
 		}
+		i++;
 	}
 	va_end(args);
-	return (count);
+	return (len);
 }
-
